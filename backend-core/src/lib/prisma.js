a979784cd6eb06bacb600ab24f0fb1fb2fadd8b2
Oshan Nilushka <oshannilushka27@gmail.com>
2026-09-20 01:03:01 +0530

    Switch Prisma to standard @prisma/client import and optimize Azure deployment scripts

--- backend-core/src/lib/prisma.js
+++ backend-core/src/lib/prisma.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/prisma/index.js';
+import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 
 const connectionString = process.env.DATABASE_URL;
